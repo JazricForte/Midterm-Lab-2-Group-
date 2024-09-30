845c67c5c63f30746e32e54dba80121c16a074ea
CCIT1028007 <CCIT1028007@lab.cict>
2024-09-30 12:17:31 +0800

    added other functions

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function RandomNumber(max) {
-    return Math.floor(Math.random() * max);
-}
-
-var player;
-
-function TryAgain() {
-    player = prompt("Select 0, 1, or 2");
-}
-
 //0 = rock
 //1 = paper
 //2 = scissor
@@ -15,70 +5,80 @@ function TryAgain() {
 //paper > rock but paper < scissor
 // scissor > paper but scissor < rock
 
-function RockButton() {
-    
+var player;
+var totalscore;
+var tie = 0;
+    var win = 0;
+    var lose = 0;
+
+function RandomNumber(max) {
+    return Math.floor(Math.random() * max);
 }
 
+function RockButton() {
+    player = 0;
+}
 
+function PaperButton() {
+    player = 1;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function ScissorButton() {
+    player = 2;
+}
 
 function PlayButton() {
     var opponent = RandomNumber(3);
 
-    TryAgain();
-    if((player != 0) && (player != 1) && (player != 2)) {
-        TryAgain();
-    }
-    else {
         if(player == opponent) {
             console.log("TIE!");
+            tie++;
         }
         else if((player == 0) && (opponent == 2)) {
             console.log("You win!");
+            win++;
         }
         else if((player == 2) && (opponent == 0)) {
             console.log("You lose!");
+            lose++;
         }
         else if((player == 1) && (opponent == 0)) {
             console.log("You win!");
+            win++;
         }
         else if((player == 0) && (opponent == 1)) {
             console.log("You lose!");
+            lose++;
         }
         else if((player == 2) && (opponent == 1)) {
             console.log("You win!");
+            win++;
         }
         else if((player == 1) && (opponent == 2)) {
             console.log("You lose!");
+            lose++;
         }
-    }
+
+    
+}
+
+function Scoreboard() {
+    var container = document.getElementById('')
+
+}
+
+function Roundcount() {
+
+}
+
+
+
+function RemoveButtons() {
+
+}
+
+
+function RetryButton() {
+
 }
 
