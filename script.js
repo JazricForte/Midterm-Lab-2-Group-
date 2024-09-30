0d97995b958016b7f42df5d3395f1f2bafd87869
JazricForte <Jazricfortexx@gmail.com>
2024-09-30 11:21:38 +0800

    The structure of the game in jsrcipt is done

--- script.js
+++ script.js
@@ -3,34 +3,9 @@ function RandomNumber(max) {
 }
 
 var player;
-var opponent = RandomNumber(3);
 
 function TryAgain() {
     player = prompt("Select 0, 1, or 2");
-}
-
-function EvaluateTheAnswers() {
-    if(player == opponent) {
-        console.log("TIE!");
-    }
-    else if((player == 0) && (opponent == 2)) {
-        console.log("You win!");
-    }
-    else if((player == 2) && (opponent == 0)) {
-        console.log("You lose!");
-    }
-    else if((player == 1) && (opponent == 0)) {
-        console.log("You win!");
-    }
-    else if((player == 0) && (opponent == 1)) {
-        console.log("You lose!");
-    }
-    else if((player == 2) && (opponent == 1)) {
-        console.log("You win!");
-    }
-    else if((player == 1) && (opponent == 2)) {
-        console.log("You lose!");
-    }
 }
 
 //0 = rock
@@ -40,11 +15,34 @@ function EvaluateTheAnswers() {
 //paper > rock but paper < scissor
 // scissor > paper but scissor < rock
 function PlayButton() {
+    var opponent = RandomNumber(3);
+
     TryAgain();
     if((player != 0) && (player != 1) && (player != 2)) {
         TryAgain();
     }
     else {
-        EvaluateTheAnswers();
+        if(player == opponent) {
+            console.log("TIE!");
+        }
+        else if((player == 0) && (opponent == 2)) {
+            console.log("You win!");
+        }
+        else if((player == 2) && (opponent == 0)) {
+            console.log("You lose!");
+        }
+        else if((player == 1) && (opponent == 0)) {
+            console.log("You win!");
+        }
+        else if((player == 0) && (opponent == 1)) {
+            console.log("You lose!");
+        }
+        else if((player == 2) && (opponent == 1)) {
+            console.log("You win!");
+        }
+        else if((player == 1) && (opponent == 2)) {
+            console.log("You lose!");
+        }
     }
 }
+
